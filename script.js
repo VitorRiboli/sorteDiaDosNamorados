dbd71d0a152da85c0e0329703cc79f87c111936e
Vitor Riboli <vitorriboli1@gmail.com>
2022-06-12 08:05:30 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function Sortear(){
+    let sorte = parseInt(Math.random() * 11); 
+    let resultado = document.getElementById("resultado");
+    
+    if(sorte == 0){
+        resultado.innerHTML = "Eu te Amo!";
+    } else if (sorte == 1) {
+        resultado.innerHTML = ""
+    } else if (sorte == 2) {
+        resultado.innerHTML = ""
+    } else if (sorte == 3) {
+        resultado.innerHTML = ""
+    } else if (sorte == 4) {
+        resultado.innerHTML = ""
+    } else if (sorte == 5) {
+        resultado.innerHTML = ""
+    } else if (sorte == 6) {
+        resultado.innerHTML = ""
+    } else if (sorte == 7) {
+        resultado.innerHTML = ""
+    } else if (sorte == 8) {
+        resultado.innerHTML = ""
+    } else if (sorte == 9) {
+        resultado.innerHTML = ""
+    } else if (sorte == 10) {
+        resultado.innerHTML = ""
+    }
+
+    
+
+};
